638a5c059db025a15f85367f7ba8dfcb03c46b10
Edson Villarroel Ricaldes <villarroel.dev@gmail.com>
2021-05-18 16:28:30 -0400

    add list product and styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ProductComponent } from './component/product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
