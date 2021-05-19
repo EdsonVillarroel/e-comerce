f9978fac9fe988e253667c6c3450b9c830aae99b
Edson Villarroel Ricaldes <villarroel.dev@gmail.com>
2021-05-19 16:27:14 -0400

    save route and header

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,41 @@
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { HomeComponent } from './component/home/home.component';
+import { ContactComponent } from './component/contact/contact.component';
+import { ProductsComponent } from './component/products/products.component';
+import { PageNotFoundComponent } from './component/page-not-found/page-not-found.component';
+import { ProductDetailComponent } from './component/product-detail/product-detail.component';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'products',
+    component: ProductsComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+  },
+  {
+    path: 'productDetail/:id',
+    component: ProductDetailComponent,
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent,
+  },
+  {
+    path: '',
+    redirectTo:'/home',
+    pathMatch:'fulls'
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ src/app/component/home/home.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import Swiper from 'swiper';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit, AfterViewInit {
+
+  constructor() { }
+
+  mySwiper: Swiper;
+
+  ngOnInit(): void {
+  }
+  ngAfterViewInit(): void {
+    //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+    //Add 'implements AfterViewInit' to the class.
+    this.mySwiper = new Swiper('.swiper-container');
+
+  }
+
+}
