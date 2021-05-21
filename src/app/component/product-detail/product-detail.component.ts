2081695da37e045d95f47914fb9345bb79862c78
Edson Villarroel Ricaldes <villarroel.dev@gmail.com>
2021-05-21 16:28:49 -0400

    route end

--- src/app/component/product-detail/product-detail.component.ts
+++ src/app/component/product-detail/product-detail.component.ts
@@ -1,25 +1,27 @@
+import { Product } from './../../model/product.model';
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute,Params} from '@angular/router';
-import {ProductsService} from './../../services/products.service'
+import { ActivatedRoute, Params } from '@angular/router';
+import { ProductsService } from './../../services/products.service';
 
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
-  styleUrls: ['./product-detail.component.css']
+  styleUrls: ['./product-detail.component.css'],
 })
 export class ProductDetailComponent implements OnInit {
-
-  constructor(private route:ActivatedRoute,
-    private productsService:ProductsService) {
-
-   }
-
-  ngOnInit(): void {
-    this.route.params.subscribe((params:Params)=>{
-      const id = params.id;
-      const product = this.productsService.getProduct(id);
-      console.log(product);
-    })
+  product: any;
+  constructor(
+    private route: ActivatedRoute,
+    private productsService: ProductsService
+  ) {
+    // this.product = Product;
   }
 
+  ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      const id = params.id;
+      this.product = this.productsService.getProduct(id);
+      console.log(this.product);
+    });
+  }
 }
